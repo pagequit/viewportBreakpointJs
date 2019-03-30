32f1cface7da72f0ff17b31e1e1ce95e29e46039
Schluffe <schluffe@localhost.localdomain>
2019-03-30 12:17:03 +0100

    Add new testdummyes

--- tests/tests.js
+++ tests/tests.js
@@ -1,3 +1,23 @@
-QUnit.test( 'hello test', function( assert ) {
+QUnit.test( 'add breakpoints test', function( assert ) {
+
+	// Add example breakpoints
+	viewport.Breakpoint.add( new viewport.Breakpoint('xl', 1200) );
+	viewport.Breakpoint.add( new viewport.Breakpoint('lg', 992) );
+	viewport.Breakpoint.add( new viewport.Breakpoint('md', 768) );
+	viewport.Breakpoint.add( new viewport.Breakpoint('sm', 576) );
+	viewport.Breakpoint.add( new viewport.Breakpoint('xs', 1) );
+
+	assert.ok( 1 == '1', 'Passed!' );
+});
+
+
+QUnit.test( 'resize test', function( assert ) {
+
+	// Change the 'viewport' to 500px.
+	window.innerWidth = 500;
+
+	// Trigger the window resize event.
+	window.dispatchEvent(new Event('resize'));
+
 	assert.ok( 1 == '1', 'Passed!' );
 });
